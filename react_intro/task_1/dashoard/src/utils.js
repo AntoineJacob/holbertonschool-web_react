bfe379736b963adc4c1baf7d9429a942ef70fba8
AntoineJacob <5646@holbertonstudents.com>
2024-08-29 10:58:44 +0200

    project react intro

---
+++ react_intro/task_1/dashoard/src/utils.js
@@ -0,0 +1,10 @@
+// utils.js
+
+export function getFullYear() {
+    const currentYear = new Date().getFullYear();
+    return currentYear;
+  }
+  
+  export function getFooterCopy(isIndex) {
+    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+  }
